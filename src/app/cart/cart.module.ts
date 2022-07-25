f0afe9a29ba3697bdf3114de4fbd3ca239590913
Eduardo Chavez <wechavez14@gmail.com>
2022-07-25 18:28:25 -0500

    feat: add cart empty state

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -4,9 +4,10 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { CartRoutingModule } from './cart-routing.module';
 import { CartListComponent } from './pages/cart-list/cart-list.component';
+import { EmptyCartComponent } from './components/empty-cart/empty-cart.component';
 
 @NgModule({
-  declarations: [CartListComponent],
+  declarations: [CartListComponent, EmptyCartComponent],
   imports: [CommonModule, CartRoutingModule, FontAwesomeModule],
 })
 export class CartModule {}
